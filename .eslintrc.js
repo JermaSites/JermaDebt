8d6247934038ab17afb451e41c1be82da259a215
poespas <jonathanvis148@gmail.com>
2020-12-30 07:36:48 +0100

    Added editor configs

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    "env": {
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 11
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+
+        "prefer-arrow-callback":  "error",
+        "newline-before-return": "error",
+
+        "no-async-promise-executor": "off",
+        "no-constant-condition": "off"
+    }
+};
