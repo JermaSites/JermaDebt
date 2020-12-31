b6b42ff53a4ad0fc5a77b0bd1feba14839b6d406
poespas <jonathanvis148@gmail.com>
2020-12-31 01:31:59 +0100

    Space <-> Tab

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
-var converter = require('number-to-words');
+var converter = require("number-to-words");
 import { titleCase } from "title-case";
-import { Flip } from 'number-flip'
+import { Flip } from "number-flip";
 
 async function readTextFile(file) {
 	let resp = await fetch(file);
@@ -9,19 +9,21 @@ async function readTextFile(file) {
 
 (async () => {
 	const result = await readTextFile("debt.txt");
-	var resultMinusNum = result.replace(/\D/g,'');
+	var resultMinusNum = result.replace(/\D/g, "");
 	var debt = Number(resultMinusNum);
 	var debtCounterNum = document.getElementById("debtCounterNum");
 	var debtCounterText = document.getElementById("debtCounterText");
-	
+
 	new Flip({
 		node: debtCounterNum,
 		from: 0,
 		to: debt,
-		separator: ',',
-		duration: 2 // second
-	})
-	
-	debtCounterText.innerHTML = titleCase(converter.toWords(debt)).replace(/, and/g, ',<br>');
-})();
+		separator: ",",
+		duration: 2, // second
+	});
 
+	debtCounterText.innerHTML = titleCase(converter.toWords(debt)).replace(
+		/, and/g,
+		",<br>"
+	);
+})();
